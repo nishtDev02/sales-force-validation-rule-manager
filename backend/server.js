d1600003a3ecaaaa018e15b58f2cb88f522ca3f1
Nisht05 <nishtkumar461@gmail.com>
2026-05-20 12:01:49 +0530

    CORS updated

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,12 @@ const axios = require("axios");
 require("dotenv").config({ path: "../.env" });
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://sales-force-validation-rule-manager.vercel.app'
+  ]
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
